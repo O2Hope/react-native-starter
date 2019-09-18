1dd0d439520620e2d21be986a02fd2ac2ecb7249
O2hope <agusvaldes2@live.com>
2019-09-17 20:50:22 -0500

    Add Images and hardcoded score

--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { Text, StyleSheet, View} from 'react-native'
+import ImageDetail from '../components/imageDetail';
+import Forest from '../../assets/forest.jpg'
+import Beach from '../../assets/beach.jpg'
+import Mountain from '../../assets/mountain.jpg'
 
 const ImageScreen = () => {
     return (
         <View>
             <Text>Image View</Text>
+            <ImageDetail title="Forest" image={Forest} score="10"/>
+            <ImageDetail title="Beach" image={Beach} score="8"/>
+            <ImageDetail title="Mountain" image={Mountain} score="5"/>
         </View>
     )
 }
